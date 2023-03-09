a8e5b13335d17cdb0e6eeb11ed5b61e7c6fd0311
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-03-09 21:21:23 +0530

    Update Music Player GUI.py

--- Music Player GUI.py
+++ Music Player GUI.py
@@ -1,20 +1,20 @@
-#pip install pygame
+
 
 import os
 import pygame
 import tkinter as tk
 from tkinter import filedialog
 
-# Create the root window
+
 root = tk.Tk()
 root.title('Music Player')
 root.geometry('400x150')
 
-# Create the frame for the buttons
+
 button_frame = tk.Frame(root)
 button_frame.pack(pady=20)
 
-# Create the 'Select Music' button
+
 def select_music():
     file_path = filedialog.askopenfilename()
     pygame.mixer.music.load(file_path)
@@ -24,7 +24,7 @@ def select_music():
 select_button = tk.Button(button_frame, text='Select Music', command=select_music)
 select_button.pack(side='left', padx=10)
 
-# Create the 'Play' button
+
 def play_music():
     if pygame.mixer.music.get_busy():
         pygame.mixer.music.pause()
@@ -36,15 +36,20 @@ def play_music():
 play_button = tk.Button(button_frame, text='Play', command=play_music)
 play_button.pack(side='left', padx=10)
 
-# Create the label for the currently playing song
+
 song_label = tk.Label(root, text='Select a song to play')
 song_label.pack()
 
 root.mainloop()
 
-# Initialize the pygame library
+
 pygame.mixer.init()
 
 
 if __name__ == '__main__':
     root.mainloop()
+
+    
+    #himanshu 
+    #install this first before execute this program
+    #pip install pygame
